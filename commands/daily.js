3ebdb2d4e3c27e6a0f3242fbef4dd286ca653efe
_ShaDoW_MaN_ <32039975+ShadwX57@users.noreply.github.com>
2018-10-12 05:39:41 +0300

    Create daily.js

---
+++ commands/daily.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args, sql) => {
+
+ let rows =  sql.prepare(`SELECT * FROM profile WHERE UserID = '${message.author.id}'`).get()
+    if(rows.length < 1) return message.reply("حاول مرة أخرى").then(msg => {msg.delete(5000)});
+  let uCoins = rows.coins;
+
+  message.channel.send(`**${message.author.username} you collect your \`200\` :dollar: daily pounds**`)
+  uCoins[message.author.id].coins += 200;
+
+
+}
+
+module.exports.help = {
+  name: "daily"
+}
